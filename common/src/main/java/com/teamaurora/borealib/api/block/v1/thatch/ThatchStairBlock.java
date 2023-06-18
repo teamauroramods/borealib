8b46e7d99528936e43dfce9d50b9453b8a394c4e
rosevcook <rose.cook969@gmail.com>
2023-06-18 14:05:52 +0100

    common thatch block classes

---
+++ common/src/main/java/com/teamaurora/borealib/api/block/v1/thatch/ThatchStairBlock.java
@@ -0,0 +1,24 @@
+package com.teamaurora.borealib.api.block.v1.thatch;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.StairBlock;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class ThatchStairBlock extends StairBlock {
+
+    public ThatchStairBlock(BlockState blockState, Properties properties) {
+        super(blockState, properties);
+    }
+
+    @Override
+    public boolean propagatesSkylightDown(BlockState state, BlockGetter reader, BlockPos pos) {
+        return true;
+    }
+
+    @Override
+    public float getShadeBrightness(BlockState state, BlockGetter worldIn, BlockPos pos) {
+        return 1.0F;
+    }
+    
+}
