12bb2fd8f5fd44e468ee7a87d45c43b43c5762b6
rosevcook <rose.cook969@gmail.com>
2023-06-18 14:00:35 +0100

    forge mod compat block classes

---
+++ forge/src/main/java/com/teamaurora/borealib/api/block/v1/compat/BookshelfBlock.java
@@ -0,0 +1,22 @@
+package com.teamaurora.borealib.api.block.v1.compat;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+
+/**
+ * bookshelf block for Quark, Woodworks, and Carpenter
+ */
+public class BookshelfBlock extends Block {
+
+    public BookshelfBlock(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public float getEnchantPowerBonus(BlockState state, LevelReader level, BlockPos pos) {
+        return 1.0F;
+    }
+
+}
